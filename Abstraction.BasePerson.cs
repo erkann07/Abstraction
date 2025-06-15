907e6ede0729d253173db9f01624ae4c96329d59
Erkan <erkankrgz0707@gmail.com>
2025-06-15 18:34:45 +0300

    Add files via upload

---
+++ Abstraction.BasePerson.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public abstract class BasePerson
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public BasePerson(string name, string lastname, string position)
+        {
+            Name = name;
+            LastName = lastname;
+            Position = position;
+        }
+
+        public abstract void Gorev();
+    }
+public class ProjeYoneticisi : BasePerson
+{
+    public ProjeYoneticisi(string ad, string soyad, string departman)
+        : base(ad, soyad, departman)
+    {
+    }
+
+    public override void Gorev()
+    {
+        Console.WriteLine("Proje yöneticisi olarak çalışıyorum.");
+    }
+}
+public class YazılımGeliştirici : BasePerson
+{
+    public YazılımGeliştirici(string ad, string soyad, string departman)
+        : base(ad, soyad, departman)
+    {
+    }
+    public override void Gorev()
+    {
+        Console.WriteLine("Yazılım geliştiricisi olarak çalışıyorum.");
+    }
+}
+public class VeriAnalisti : BasePerson
+{
+    public VeriAnalisti(string ad, string soyad, string departman)
+        : base(ad, soyad, departman)
+    {
+    }
+    public override void Gorev()
+    {
+        Console.WriteLine("Veri analisti olarak çalışıyorum.");
+    }
+}
+
+
---
+++ Abstraction.Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+ProjeYoneticisi ProjeYoneticisi1 = new ProjeYoneticisi("Hasan", "Çıldırmış", "Proje Yönetimi");
+BasePerson YazılımGelistiricisi1 = new YazılımGeliştirici("Murat", "Demir", "Yazılım Geliştirme");
+BasePerson VeriAnalisti1 = new VeriAnalisti("Ayşe", "Yılmaz", "Veri Analizi");
+Console.WriteLine("Sorgudan çıkmak için Q tuşuna basınız.");
+
+while (true)
+{
+    Console.Write("Sorgulamak istediğiniz kişiyi seçiniz(Hasan , Murat , Ayşe) :");
+    string yanıt = Console.ReadLine().ToLower();
+    if (yanıt == "hasan")
+        ProjeYoneticisi1.Gorev();
+    else if (yanıt == "murat")
+    {
+        YazılımGelistiricisi1.Gorev();
+    }
+    else if (yanıt == "ayşe" ||yanıt == "ayse")
+    {
+        VeriAnalisti1.Gorev();
+    }
+    else if (yanıt == "q")
+    {
+        Console.WriteLine("Sorgudan çıkılıyor...");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Geçersiz giriş. Lütfen Hasan, Murat veya Ayşe giriniz.");
+    }
+}
